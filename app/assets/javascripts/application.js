4eb55d552043258433da0a51a3aca37dd8a5bae0
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-07-05 12:14:45 -0700

    completed current and past job editing on user profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,8 +25,36 @@ window.onload = function(){
 		$("#allPictures1").show();
 	});
 	$("#button2").on("click", function() {
-		alert("hi")
+		$("#allPictures2").show();
 	});
+	$("#timeInOne").on("click", function() {
+		var now = new Date();
+		var date = now.toString();
+		var currentDate = date.slice(0,15);
+		var currentTime = date.slice(16,24) 
+		$("#currentDate").html(currentDate);
+		$("#timeIn").text(currentTime);
+	});
+	$("#timeOutOne").on("click", function() {
+		var now = new Date();
+		var date = now.toString();
+		var currentTime = date.slice(16,24); 
+		$("#timeOut").text(currentTime);	
+	});
+	$("#hideBtn").on("click", function() {
+		console.log("#allPictures1");
+		$("#allPictures1").toggle();
+	});
+	$("#hideBtn2").on("click", function() {
+		$("#allPictures2").toggle();
+	});	
+	$("#jobComp").on("click", function() {
+		var job = $("#vegasStrip").html();
+		$("#vegasStrip").html("No current job");
+		$("#vegasStrip2").html(job);
+		$(".clearTime").html("");
+	});
+
 };
 
 
